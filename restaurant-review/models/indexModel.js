990da9add60bc7993e051dfda8901a0a7a1169de
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-23 22:48:02 -0400

    able to convert an array type in postgres to json

--- restaurant-review/models/indexModel.js
+++ restaurant-review/models/indexModel.js
@@ -1,13 +1,14 @@
 const db = require("./conn");
 
 class Restaurant {
-    constructor(name, street, city, state, zipcode, picture) {
+    constructor(name, street, city, state, zipcode, picture, array_data) {
         this.name = name;
         this.street = street;
         this.city = city;
         this.state = state;
         this.zipcode = zipcode;
         this.picture = picture;
+        this.array_data = array_data;
     }
 
     static async getAll() {
@@ -26,6 +27,42 @@ class Restaurant {
                 [id]
             );
             return response;
+        } catch (err) {
+            return err.message;
+        }
+    }
+
+    // don't think this needs to be async as this is a helper/util function. Data already retrieved in getArrayAnswer(id).
+    
+    static convertArrayDataToObj(obj) {
+        const objectToConvert = obj;
+        const objLength = objectToConvert.length;
+        let objAnswer = new Object();
+
+        // Assuming the array in the database will ALWAYS be an even length and the answers are in pairs e.g a, 3, b, 7
+        // This loop dynamically travels the length of the array data and populates key/value pairs and returns an object
+        for (let i = 0; i < objLength; i += 2) {
+            let key = objectToConvert[i];
+            let value = objectToConvert[i + 1];
+
+            objAnswer[key] = value;
+        }
+
+        return objAnswer;
+    }
+
+    static async getArrayAnswer(id) {
+        // SQL starts counting from 1 instead of 0
+        try {
+            const response = await db.one(
+                `SELECT array_data FROM restaurants WHERE id = $1;`,
+                [id]
+            );
+            const arrayObj = response.array_data;
+            const convertedAnswer = this.convertArrayDataToObj(arrayObj);
+
+            return convertedAnswer;
+
         } catch (err) {
             return err.message;
         }
--- restaurant-review/routes/indexController.js
+++ restaurant-review/routes/indexController.js
@@ -20,17 +20,27 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:restaurant_id", async (req, res, next) => {
     const { restaurant_id } = req.params;
-    const theRestaurant = await restaurantClass.getById(restaurant_id);
+    // const theRestaurant = await restaurantClass.getById(restaurant_id);
+    // Array Data Test Code
+    const theRestaurantArray = await restaurantClass.getArrayAnswer(
+        restaurant_id
+    );
 
-    res.render("template", {
-        locals: {
-            title: "This is one restaurant",
-            restaurantData: theRestaurant
-        },
-        partials: {
-            partial: "partial-single-restaurant"
-        }
-    });
+    const theAnswerObj = theRestaurantArray;
+    console.log("Testing if I passed the answer obj from the model", theAnswerObj);
+    
+    // For testing purposes, don't care about seeing anything in the browser
+
+    // res.render("template", {
+    //     locals: {
+    //         title: "This is one restaurant",
+    //         restaurantData: theRestaurant,
+    //         isLoggedIn: req.session.is_logged_in
+    //     },
+    //     partials: {
+    //         partial: "partial-single-restaurant"
+    //     }
+    // });
 });
 
 module.exports = router;
